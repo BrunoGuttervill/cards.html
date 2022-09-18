3dcd4b30ecf5746ae933fda08bf4ede181686b9f
Bruno Guttervill <108041441+BrunoGuttervill@users.noreply.github.com>
2022-09-18 20:38:51 -0300

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const camisa1 = document.getElementById('camisa1');
+const camisa2 = document.getElementById('camisa2');
+const camisa3 = document.getElementById('camisa3');
+
+camisa1.addEventListener("click", () =>{
+    window.alert('Comprar CAMISA MANGA LONGA 1981 por R$ 289,90')
+})
+camisa2.addEventListener("click", () =>{
+    window.alert('Comprar CAMISA MODELO 2, 2019 por R$ 299,90')
+})
+camisa3.addEventListener("click", () =>{
+    window.alert('Comprar UNIFORME 1, FLAMENGO 2022 por R$ 399,90')
+})
